77b83b9973a24491fda5a6734a4a79d0414f7b97
Olena Zaritska <zaritskaos@gmail.com>
2024-05-25 13:59:07 +0300

    implement py-restore-names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,48 @@
-import pytest
+from unittest import TestCase
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+class TestUser(TestCase):
+    def setUp(self) -> None:
+        self.users = [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+        ]
+
+    def test_first_name_present(self) -> None:
+        restore_names(self.users)
+        assert self.users == [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+        ]
+
+    def test_first_name_none(self) -> None:
+        for user in self.users:
+            user["first_name"] = None
+        restore_names(self.users)
+        assert self.users == [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+        ]
+
+    def test_first_name_key_absent(self) -> None:
+        for user in self.users:
+            del user["first_name"]
+        restore_names(self.users)
+        assert self.users == [
+            {
+                "first_name": "Jack",
+                "last_name": "Holy",
+                "full_name": "Jack Holy",
+            },
+        ]
